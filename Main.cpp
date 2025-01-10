0a33e5de6978ad38cfa2f9f2477d811ff791361e
Ethan-Mulwee <167151595+Ethan-Mulwee@users.noreply.github.com>
2025-01-10 14:13:48 -0500

    clean up main

--- Main.cpp
+++ Main.cpp
@@ -52,82 +52,28 @@ int main() {
     body2.collider = collider2;
     world.bodies.push_back(body2);
     ///////////////////////////////////////////////////////////////////////////////////////
-
-    bMath::float3 bodyPoint;
-    bMath::float3 dragPoint;
-    bool dragging = false;
-
-    bMath::float2 mouseDeltaPos;
-
-    bEngine::Contact testContact;
-    testContact.contactNormal = bMath::normalized(bMath::float3(0.0,0.0,1.0));
+    std::cout << "before: " << world.bodies[0].orientation.angle() << "\n";
+    world.bodies[0].orientation = rotate2(bMath::quaternion(1,0,0,0), bMath::float3(100,0,0));
+    std::cout << world.bodies[0].orientation << "\n";
+    std::cout << "after: " << world.bodies[0].orientation.angle() << "\n";
 
     while(!WindowShouldClose()) {
-        mouseDeltaPos = toBread(GetMouseDelta());
 
-        // Ray screenRay = GetScreenToWorldRay(GetMousePosition(), camera);
-
-        // if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT) && !IsKeyDown(KEY_LEFT_CONTROL)) {
-        //     if (collision.hit) {
-        //         bodyPoint = block.body->positionToBodySpace((toBread(collision.point)));
-        //         dragPoint = toBread(collision.point);
-        //         dragging = true;
-        //     }
-        // }
-
-        // if (IsKeyDown(KEY_LEFT_CONTROL) && !dragging) {
-        //     if (collision.hit) {
-        //         bodyPoint = block.body->positionToBodySpace((toBread(collision.point)));
-        //         bMath::float3 worldSpaceBodyPoint = bodyPoint*body->getTransform();
-        //         block.body->addForceAtPoint(toBread(collision.normal)*-4, worldSpaceBodyPoint);
-        //     }
-        // }
-
-        // if (IsMouseButtonReleased(MOUSE_BUTTON_LEFT)) {
-        //     dragging = false;
-        // }
-
-
-        // if (dragging) {
-        //     bMath::float3 viewVector = bMath::normalized(toBread(camera.target) - toBread(camera.position));
-        //     bMath::float3 tagentViewVector = bMath::normalized(bMath::cross(toBread(camera.up), viewVector));
-        //     bMath::float3 cameraUpVector = bMath::cross(viewVector, tagentViewVector);
-        //     bMath::matrix3 spaceRotation(
-        //       tagentViewVector.x, cameraUpVector.x, viewVector.x,
-        //       tagentViewVector.y, cameraUpVector.y, viewVector.y,
-        //       tagentViewVector.z, cameraUpVector.z, viewVector.z
-        //     );
-        //     dragPoint += bMath::float3(-mouseDeltaPos.x*0.005, -mouseDeltaPos.y*0.005, 0)*spaceRotation;
-        //     bMath::float3 worldSpaceBodyPoint = bodyPoint*body->getTransform();
-        //     bMath::float3 force = dragPoint - worldSpaceBodyPoint;
-        //     block.body->addForceAtPoint(force, worldSpaceBodyPoint);
-        // }
-
-        world.bodies[0].addForceAtBodyPoint(bMath::float3(0,-1,0), bMath::float3(1,1,1));
-        world.bodies[1].addForceAtBodyPoint(bMath::float3(0,-.2,0), bMath::float3(0,1,0));
+        // world.bodies[0].addForceAtBodyPoint(bMath::float3(0,-1,0), bMath::float3(1,1,1));
+        // world.bodies[1].addForceAtBodyPoint(bMath::float3(0,-.2,0), bMath::float3(0,1,0));
         world.step(1/60.0f);
 
-        // testContact.contactNormal = bMath::rotateZ(testContact.contactNormal, 0.1f);
 
         UpdateCamera(&camera, CAMERA_ORBITAL);
         BeginDrawing();
             ClearBackground(Color{35,35,35,255});
             BeginMode3D(camera);
                 DrawGrid(5,1);
-                // DrawLine3D(Vector3{-axisLength,0,0}, Vector3{axisLength,0,0}, RED);
-                // DrawLine3D(Vector3{0,-axisLength,0}, Vector3{0,axisLength+0.5,0}, GREEN);
-                // DrawLine3D(Vector3{0,0,-axisLength}, Vector3{0,0,axisLength}, BLUE);
+                DrawLine3D(Vector3{-axisLength,0,0}, Vector3{axisLength,0,0}, RED);
+                DrawLine3D(Vector3{0,-axisLength,0}, Vector3{0,axisLength+0.5,0}, GREEN);
+                DrawLine3D(Vector3{0,0,-axisLength}, Vector3{0,0,axisLength}, BLUE);
 
-                // block.render();
-                // if (collision.hit)
-                //     DrawSphere(collision.point, 0.1, PURPLE);
-                // if (dragging) {
-                //     DrawSphere(toRay(bodyPoint*body->getTransform()), 0.1,RED);
-                //     DrawSphere(toRay(dragPoint), 0.1, ORANGE);
-                //     DrawLine3D(toRay(bodyPoint*body->getTransform()), toRay(dragPoint), BLACK);
-                // }
-                drawBasis(testContact.getContactBasis());
-                // renderer.render(world);
+                renderer.render(world);
             EndMode3D();
         EndDrawing();
     }
