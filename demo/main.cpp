7484fd8cd996a08aa179b6e32726941bb9032ff7
Ethan-Mulwee <167151595+Ethan-Mulwee@users.noreply.github.com>
2025-06-07 13:00:52 -0400

    window

--- demo/main.cpp
+++ demo/main.cpp
@@ -1,5 +1,27 @@
 #include <iostream>
+#include <GLFW/glfw3.h>
 
 int main() {
   std::cout << "Hello World! \n";
+
+  GLFWwindow* window;
+  if (!glfwInit()) {
+    fprintf(stderr, "Failed to intialize GLFW\n");
+    exit( EXIT_FAILURE );
+  }
+
+  window = glfwCreateWindow(500,500, "Window", NULL, NULL);
+
+  if (!window) {
+    fprintf(stderr , "Failed to open GLFW window \n");
+    glfwTerminate();
+    exit( EXIT_FAILURE);
+  }
+
+  while (!glfwWindowShouldClose(window)) {
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+  }
+
+  glfwTerminate();
 }
