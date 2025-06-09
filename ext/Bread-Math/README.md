3d29b07fcf280b8a039e0ea42535b091ce37c42c
Ethan-Mulwee <167151595+Ethan-Mulwee@users.noreply.github.com>
2025-06-09 09:46:21 -0400

    fix bMath

--- ext/Bread-Math/README.md
+++
@@ -1,2 +0,0 @@
-# Bread Math
-A C++ header only template library which is not particularly good, but it's mine 
--- ext/Bread-Math/include/bMath/common.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef BMATH_COMMON
-#define BMATH_COMMON
-
-// includes the common operations and types
-
-#include "vector.hpp"
-#include "matrix.hpp"
-
-#endif
