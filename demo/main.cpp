04207cb6394547266c1798c161152f44ea3b3ceb
Ethan-Mulwee <167151595+Ethan-Mulwee@users.noreply.github.com>
2025-07-25 14:52:17 -0400

    working window

--- demo/main.cpp
+++ demo/main.cpp
@@ -1,7 +1,66 @@
-#include "application.hpp"
+#include <iostream>
+
+#include "GL/glew.h"
+#include "GLFW/glfw3.h"
+
+#include "imgui.h"
+#include "imgui_impl_glfw.h"
+#include "imgui_impl_opengl3.h"
+
+#include "bMath.hpp"
+#include "ext/iostream.hpp"
+
+
+GLFWwindow* createWindow(int width, int height, const char* name) {
+    const char* glsl_version = "#version 130";
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+
+    if (!glfwInit()) {
+        fprintf(stderr, "Failed to intialize GLFW\n");
+        exit( EXIT_FAILURE );
+    }
+
+    GLFWwindow* glfwWindow = glfwCreateWindow(width, height, name, NULL, NULL);
+
+    if (!glfwWindow) {
+        fprintf(stderr , "Failed to open GLFW window \n");
+        glfwTerminate();
+        exit( EXIT_FAILURE);
+    }
+
+    glfwMakeContextCurrent(glfwWindow);
+    glfwSwapInterval(1); // Enable vsync
+
+    // glfwSetScrollCallback(glfwWindow, onScrollCallback);
+
+    glewInit();
+
+    std::cout << glGetString(GL_VERSION) << std::endl;
+
+    return glfwWindow;
+}
+
+void updateWindow(GLFWwindow* glfwWindow) {
+    glfwSwapBuffers(glfwWindow);
+    glfwPollEvents();
+}
+
+void destroyWindow(GLFWwindow* glfwWindow) {
+    // ImGui_ImplOpenGL3_Shutdown();
+    // ImGui_ImplGlfw_Shutdown();
+    // ImGui::DestroyContext();
+
+    glfwDestroyWindow(glfwWindow);
+    glfwTerminate(); 
+}
 
 int main() {
-    Application app(1920, 1080, "test");
+    GLFWwindow* glfwWindow = createWindow(500, 500, "window"); 
 
-    app.loop();
+    while(!glfwWindowShouldClose(glfwWindow)) { 
+        updateWindow(glfwWindow);
+    }
+
+    destroyWindow(glfwWindow);
 }
