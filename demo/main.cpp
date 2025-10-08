c0fdaebd450440aa91f6b84b0fc035079baffe60
Ethan-Mulwee <167151595+Ethan-Mulwee@users.noreply.github.com>
2025-10-07 22:10:23 -0400

    spliting code

--- demo/main.cpp
+++ demo/main.cpp
@@ -4,117 +4,15 @@
 #include <vector>
 #include <cstring>
 
-#include "GL/glew.h"
-#include "GLFW/glfw3.h"
-
-#include "bphysics/contacts.hpp"
-#include "imgui.h"
-#include "imgui_impl_glfw.h"
-#include "imgui_impl_opengl3.h"
-
-#include "matrix_function.hpp"
-#include "matrix_type.hpp"
-#include "quaternion_type.hpp"
 #include "smath.hpp"
 #include "smath_iostream.hpp"
 
 #include "bphysics/world.hpp"
+#include "bphysics/contacts.hpp"
 #include "bphysics/interia_tensor.hpp"
 #include "bphysics/helper.hpp"
 
-/* -------------------------------------------------------------------------- */
-/*                                 GLFW Window                                */
-/* -------------------------------------------------------------------------- */
-
-struct GLWindow {
-    GLFWwindow* glfwWindow;
-    int width, height;
-    const char* name;
-    
-    smath::vector2 mousePos = {0.0f,0.0f};
-    smath::vector2 deltaMousePos = {0.0f,0.0f};
-    float scrollInput = 0.0f;
-    float time = 0.0f;
-    float deltaTime = 0.0f;
-    double perviousRenderTime = 0.0;
-    double perviousPhysicsTime = 0.0;
-};
-
-void scrollCallback(GLFWwindow* window, double xoffset, double yoffset) {
-    std::cout << xoffset << ", " << yoffset << "\n";
-}
-
-GLWindow* createWindow(int width, int height, const char* name) {
-
-    GLWindow* window = new GLWindow();
-
-    window->width = width;
-    window->height = height;
-    window->name = name;
-
-    const char* glsl_version = "#version 130";
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
-
-    if (!glfwInit()) {
-        fprintf(stderr, "Failed to intialize GLFW\n");
-        exit( EXIT_FAILURE );
-    }
-
-    window->glfwWindow = glfwCreateWindow(width, height, name, NULL, NULL);
-
-    if (!window->glfwWindow) {
-        fprintf(stderr , "Failed to open GLFW window \n");
-        glfwTerminate();
-        exit( EXIT_FAILURE);
-    }
-
-    glfwMakeContextCurrent(window->glfwWindow);
-    glfwSwapInterval(1); // Enable vsync
-
-    // glfwSetScrollCallback(glfwWindow, onScrollCallback);
-
-    glewInit();
-
-    std::cout << glGetString(GL_VERSION) << std::endl;
-    std::cout << glGetString(GL_RENDERER) << std::endl;
-
-    glfwSetWindowUserPointer(window->glfwWindow, window);
-    glfwSetScrollCallback(window->glfwWindow, [](GLFWwindow* window, double xoffset, double yoffset) { 
-        GLWindow* windowA = (GLWindow*)glfwGetWindowUserPointer(window);
-        windowA->scrollInput = yoffset; 
-    });
-
-    return window;
-}
-
-
-void updateWindow(GLWindow* window) {
-    window->scrollInput = 0.0f;
-
-    glfwSwapBuffers(window->glfwWindow);
-    glfwPollEvents();
-
-    double x, y;
-    glfwGetCursorPos(window->glfwWindow, &x, &y);
-    smath::vector2 currentPos = smath::vector2{(float)x,(float)y};
-    window->deltaMousePos = currentPos - window->mousePos;
-    window->mousePos = currentPos;
-
-    float currentTime = glfwGetTime();
-    window->deltaTime = currentTime - window->time;
-    window->time = currentTime;
-}
-
-void destroyWindow(GLWindow* window) {
-    ImGui_ImplOpenGL3_Shutdown();
-    ImGui_ImplGlfw_Shutdown();
-    ImGui::DestroyContext();
-
-    glfwDestroyWindow(window->glfwWindow);
-    delete window;
-    glfwTerminate(); 
-}
+#include "window.hpp"
 
 /* -------------------------------------------------------------------------- */
 /*                                    ImGui                                   */
---
+++ demo/window.hpp
@@ -0,0 +1,35 @@
+#ifndef DEMO_WINDOW
+#define DEMO_WINDOW
+
+#include <iostream>
+
+#include "GL/glew.h"
+#include "GLFW/glfw3.h"
+
+#include "imgui.h"
+#include "imgui_impl_glfw.h"
+#include "imgui_impl_opengl3.h"
+
+#include "smath.hpp"
+
+struct GLWindow {
+    GLFWwindow* glfwWindow;
+    int width, height;
+    const char* name;
+    
+    smath::vector2 mousePos = {0.0f,0.0f};
+    smath::vector2 deltaMousePos = {0.0f,0.0f};
+    float scrollInput = 0.0f;
+    float time = 0.0f;
+    float deltaTime = 0.0f;
+    double perviousRenderTime = 0.0;
+    double perviousPhysicsTime = 0.0;
+};
+
+GLWindow *createWindow(int width, int height, const char *name);
+
+void updateWindow(GLWindow *window);
+
+void destroyWindow(GLWindow *window);
+
+#endif
